b970467951a5607bb5127a031fc06350efb9b8ae
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-03-27 09:05:38 -0300

    primeros cambios

---
+++ Jugador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio-Diccionarios-mDorego
+{
+
+class Jugador
+{
+
+private string nombre;
+private Dictionary<string, int> inventario; //string es el NOMBRE DEL MATERIAL, es la KEY. -- int es la CANTIDAD del material, es el VALOR.
+
+public Jugador(string nombre)
+{
+    this.nombre = nombre;
+    this.inventario = new Dictionary <string, int>();
+}
+
+}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicio-Diccionarios-mDorego
+{
+
+internal class Program
+{
+
+static void Main(string[] args)
+{
+
+Jugador jugador1 = new Jugador("jugador1")
+bool salir = false;
+    Console.WriteLine("Bienvenido a SuperSupervivencia version 1.0\n");
+        while (salir == false)
+        {
+        Console.WriteLine("Que haras ahora?\n\n" +
+                          "1 - Ver tu inventario \n" +
+                          "2 - Recoger material \n"+
+                          "3 - Usar material \n" +
+                          "4 - Inspecionar material \n" +
+                          "5 - Salir");
+                          // opcion 4 deberia mostrar cuanto tenes de un material que le pidas. eso nadamas.
+
+        string opcion = Console.ReadLine();
+
+
+
+
+        }
+}
+
+}
+
+}
